6343ac3d6e0784d30a5f5b872d780fe6e5a13fa6
Lakshay <lakshay2876@gmail.com>
2026-01-24 23:39:51 +0530

    Cors updated for frontend API calls

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://verity-frontend.vercel.app"
+    ],
     credentials: true,
   })
 );
