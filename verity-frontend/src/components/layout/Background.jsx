40c6a911c878176f72bb2a870372c6e37ea69709
Lakshay <lakshay2876@gmail.com>
2026-02-24 02:02:01 +0530

    Revert "Background changed to white"
    This reverts commit 1fc653ca9ac84b3b8de0699d5a316d40a860926c.

--- verity-frontend/src/components/layout/Background.jsx
+++ verity-frontend/src/components/layout/Background.jsx
@@ -1,6 +1,16 @@
 
 const Background = ({ children }) => {
-  
+    
+  return (
+    <Aurora
+  colorStops={["#7cff67","#B19EEF","#5227FF"]}
+  blend={0.2}
+  amplitude={1.0}
+  speed={1}
+>
+      {children}
+      </Aurora>
+  );
 };
 
 export default Background;
