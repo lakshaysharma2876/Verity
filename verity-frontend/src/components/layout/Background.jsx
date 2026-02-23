1fc653ca9ac84b3b8de0699d5a316d40a860926c
Lakshay <lakshay2876@gmail.com>
2026-02-24 01:55:12 +0530

    Background changed to white

--- verity-frontend/src/components/layout/Background.jsx
+++ verity-frontend/src/components/layout/Background.jsx
@@ -4,14 +4,11 @@ import Aurora from './Aurora';
 const Background = ({ children }) => {
     
   return (
-    <Aurora
-  colorStops={["#7cff67","#B19EEF","#5227FF"]}
-  blend={0.2}
-  amplitude={1.0}
-  speed={1}
->
-      {children}
-      </Aurora>
+    {
+      style : {
+        background:white,
+      }
+    }
   );
 };
 
