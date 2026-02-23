8c7eff8645847da4c159160301bee8c1107168fc
Lakshay <lakshay2876@gmail.com>
2026-02-24 02:00:04 +0530

    Background changed to white

--- verity-frontend/src/components/layout/Background.jsx
+++ verity-frontend/src/components/layout/Background.jsx
@@ -1,15 +1,6 @@
-import Aurora from './Aurora';
-  
 
 const Background = ({ children }) => {
-    
-  return (
-    <div
-      style = {{
-        backgroundColor:"white",
-      }}
-      ></div>
-  );
+  
 };
 
 export default Background;
